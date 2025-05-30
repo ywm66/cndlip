35980f98856528b80b292726b402246fe89bbfba
ywm66 <111097371+ywm66@users.noreply.github.com>
2025-05-31 01:17:32 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+export default {
+  async fetch(request, env, ctx) {
+    // 手动用 \n 换行
+    const text = ".rainyun.com\n*.*.rainyun.com\n";
+
+    return new Response(text, {
+      headers: {
+        // 关键：纯文本
+        "Content-Type": "text/plain; charset=utf-8",
+        // 禁止缓存，方便调试
+        "Cache-Control": "no-cache, no-store, must-revalidate"
+      }
+    });
+  }
+}
